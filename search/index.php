48b98c46dc214eb3b58e05ab9ced8650606f767d
englishextra <englishextra@yandex.ru>
2017-01-16 02:41:56 +0300

    removed user-scalable=no

--- search/index.php
+++ search/index.php
@@ -349,7 +349,7 @@ $SQLITE_CACHE = null;
 <html lang="ru">
 	<head>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="robots" content="noindex,nofollow" />
 		<title>Поиск</title>
 		<!-- <link rel="stylesheet" href="../libs/search/css/bundle.min.css" /> -->
--- tools/showexternalcounters.php
+++ tools/showexternalcounters.php
@@ -192,7 +192,7 @@ if ($event == 'clear') {
 <html lang="ru">
 	<head>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<meta name="robots" content="noindex,nofollow" />
 		<title>Журнал посещений</title>
 		<!-- <link rel="stylesheet" href="../libs/admin/css/bundle.min.css" /> -->
